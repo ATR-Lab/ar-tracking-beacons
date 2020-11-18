78bd2f0c3875f996be0e83f6c07e54f0dd112b15
KalebP <kalebpowlison@gmail.com>
2020-11-18 15:12:40 -0500

    Adding limited tracked object selection, progress on fixing compass

--- Assets/Scripts/TargetSelector.cs
+++ Assets/Scripts/TargetSelector.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class TargetSelector : MonoBehaviour
 {
+
+    [SerializeField]
+    private GameObject _target1, _target2;
+
+    public GameObject targetObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetObject = _target1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (targetObject = _target1)
+            {
+                targetObject = _target2;
+            }
+            else targetObject = _target1;
+        }
     }
 }
--- Assets/Scripts/UITracking.cs
+++ Assets/Scripts/UITracking.cs
@@ -5,34 +5,55 @@ using UnityEngine;
 public class UITracking : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _trackedObject, _targetUI, _needle;
+    private GameObject _targetUI, _needle, _target1, _target2;
     [SerializeField]
     private Camera _camera;
+
+    private GameObject _trackedObject;
+  
 
     /*      _trackedObject = object being tracked
      *      _targetUI = UI targeting box
      *      _needle = UI compass needle 
-     */     
+     */
+
+   
 
     private void Start()
     {
 
         _camera = transform.Find("Main Camera").GetComponent<Camera>();
+        _trackedObject = _target1;
 
-        
+
+
     }
 
     void Update()
     {
 
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (_trackedObject == _target1)
+            {
+                _trackedObject = _target2;
+            }
+            else
+            {
+                _trackedObject = _target1;
+            }
+        }
+
         //Targeting box code
         Vector3 screenPos = _camera.WorldToScreenPoint(_trackedObject.transform.position);
         _targetUI.transform.position = screenPos;
 
-        //Compass code
-        Vector3 newDirection = Vector3.RotateTowards(_needle.transform.position, _trackedObject.transform.position - transform.position, 2, 2);
+        //Compass 
+        Vector3 newDirection = Vector3.RotateTowards(_needle.transform.position, _trackedObject.transform.position/* - transform.position*/, 4, 4);
         Debug.DrawRay(_needle.transform.position, newDirection, Color.red);
-        _needle.transform.rotation = Quaternion.LookRotation(newDirection);
+        Quaternion newAngle = Quaternion.Euler(newDirection.x, newDirection.y, newDirection.z);
+        _needle.transform.rotation = newAngle;
+        
 
     }
 
